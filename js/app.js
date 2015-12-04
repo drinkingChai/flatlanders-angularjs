36d0f9d25767c4468957c5524a6e2b2117aa7faa
drinkingTea <teatocode@gmail.com>
2015-12-04 00:34:10 -0500

    Updated to lesson 3

--- js/app.js
+++ js/app.js
@@ -2,33 +2,41 @@
 	var app = angular.module('store', [ ]);
 
 	app.controller('StoreController', function() {
-		this.products = gems;
-	});
+		this.products = [
+			{
+				name: 'Dodecahedron',
+				price: 2.95,
+				description: '...',
+				canPurchase: true,
+				images: [
+					'img/gem-02.gif',
+					'img/gem-05.gif',
+					'img/gem-09.gif'
+				],
+				reviews: [
+					{
 
-	var gems = [
-		{
-			name: 'Dodecahedron',
-			price: 2.95,
-			description: '...',
-			canPurchase: true,
-			images: [
-				'img/gem-02.gif',
-				'img/gem-05.gif',
-				'img/gem-09.gif'
-			]
-		},
-		{
-			name: 'Pentagonal Gem',
-			price: 5.95,
-			description: '...',
-			canPurchase: false,
-			images: [
-				'img/gem-01.gif',
-				'img/gem-03.gif',
-				'img/gem-04.gif'
-			]
-		}
-	]
+					}
+				]
+			},
+			{
+				name: 'Pentagonal Gem',
+				price: 5.95,
+				description: '...',
+				canPurchase: false,
+				images: [
+					'img/gem-01.gif',
+					'img/gem-03.gif',
+					'img/gem-04.gif'
+				],
+				reviews: [
+					{
+						
+					}
+				]
+			}
+		]
+	});
 
 	app.controller('PanelController', function() {
 		this.tab = 1;
@@ -39,6 +47,16 @@
 
 		this.isSelected = function(checkTab) {
 			return this.tab === checkTab;
+		}
+	});
+
+	app.controller('ReviewController', function() {
+		this.review = {};
+
+		this.addReview = function(product) {
+			this.review.createdOn = Date.now();
+			product.reviews.push(this.review);
+			this.review = {};
 		}
 	});
 
