97152c730fd0d2c6c32ca9950907e7ec8532c46f
drinkingTea <teatocode@gmail.com>
2015-12-03 20:16:19 -0500

    updated to lesson 2.5

--- js/app.js
+++ js/app.js
@@ -2,12 +2,32 @@
 	var app = angular.module('store', [ ]);
 
 	app.controller('StoreController', function() {
-		this.product = gem;
+		this.products = gems;
 	});
 
-	var gem = {
-		name: 'Dodecahedron',
-		price: 2.95,
-		description: '...'
-	}
+	var gems = [
+		{
+			name: 'Dodecahedron',
+			price: 2.95,
+			description: '...',
+			canPurchase: true,
+			images: [
+				'img/gem-02.gif',
+				'img/gem-05.gif',
+				'img/gem-09.gif'
+			]
+		},
+		{
+			name: 'Pentagonal Gem',
+			price: 5.95,
+			description: '...',
+			canPurchase: false,
+			images: [
+				'img/gem-01.gif',
+				'img/gem-03.gif',
+				'img/gem-04.gif'
+			]
+		}
+	]
+
 })();
