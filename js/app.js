9e5399f543401dc0ca8a97de85d01352bc8d0d39
drinkingTea <teatocode@gmail.com>
2015-12-05 11:21:44 -0500

    custom directives

--- js/app.js
+++ js/app.js
@@ -38,18 +38,6 @@
 		]
 	});
 
-	app.controller('PanelController', function() {
-		this.tab = 1;
-
-		this.selectTab = function(setTab) {
-			this.tab = setTab;
-		}
-
-		this.isSelected = function(checkTab) {
-			return this.tab === checkTab;
-		}
-	});
-
 	app.controller('ReviewController', function() {
 		this.review = {};
 
@@ -58,6 +46,32 @@
 			product.reviews.push(this.review);
 			this.review = {};
 		}
+	});
+
+	app.directive('productTitle', function() {
+		return {
+			restrict: 'E',
+			templateUrl: 'snippets/product-title.html'
+		};
+	});
+
+	app.directive('productPanels', function() {
+		return {
+			restrict: 'E',
+			templateUrl: 'snippets/product-panels.html',
+			controller: function() {
+				this.tab = 1;
+
+				this.selectTab = function(setTab) {
+					this.tab = setTab;
+				}
+
+				this.isSelected = function(checkTab) {
+					return this.tab === checkTab;
+				}
+			},
+			controllerAs: 'panel'
+		};
 	});
 
 })();
