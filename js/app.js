7f70e1ef5fa1d612de13b2519c92629afdb56f89
drinkingTea <teatocode@gmail.com>
2015-12-06 01:38:58 -0500

    Refactored store-products into a module

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 (function() {
-	var app = angular.module('store', [ ]);
+	var app = angular.module('store', ['store-products']);
 
 	app.controller('StoreController', function() {
 		this.products = [
@@ -46,32 +46,6 @@
 			product.reviews.push(this.review);
 			this.review = {};
 		}
-	});
-
-	app.directive('productTitle', function() {
-		return {
-			restrict: 'E',
-			templateUrl: 'snippets/product-title.html'
-		};
-	});
-
-	app.directive('productPanels', function() {
-		return {
-			restrict: 'E',
-			templateUrl: 'snippets/product-panels.html',
-			controller: function() {
-				this.tab = 1;
-
-				this.selectTab = function(setTab) {
-					this.tab = setTab;
-				}
-
-				this.isSelected = function(checkTab) {
-					return this.tab === checkTab;
-				}
-			},
-			controllerAs: 'panel'
-		};
 	});
 
 })();
