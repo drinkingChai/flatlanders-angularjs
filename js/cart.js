394dd8d303cb1a496b95c37eb3d4f60e37370512
drinkingTea <teatocode@gmail.com>
2015-12-10 01:55:08 -0500

    added quantity

--- js/cart.js
+++ js/cart.js
@@ -8,21 +8,40 @@
 			templateUrl: 'snippets/cart.html',
 			controller: function() {
 				this.cart = [];
-				this.total = 0;
 
 				this.addToCart = function(product) {
 					//add quantity
-					this.cart.push(product);
-					this.total += product.price;
+					var notFoundInCart = true;
+					for (item in this.cart) {
+						//searches to see if the product is already in this.cart
+						if (this.cart[item].name === product.name) {
+							this.cart[item].quantity += product.addNewQuantity;
+							notFoundInCart = false;
+						}
+					}
+
+					if (notFoundInCart) {
+						//if the product is not in this.cart, add it
+						product.quantity = product.addNewQuantity;
+						this.cart.push(product);
+					}
+
+					//reset addNewQuantity
+					product.addNewQuantity = 1;
 				}
 
-				this.updateCart = function(product, newQuantity) {
-
+				this.updateTotal = function() {
+					//calculate total
+					total = 0;
+					for (item in this.cart) {
+						total += this.cart[item].price * this.cart[item].quantity;
+					}
+					return total;
 				}
 
 				this.removeFromCart = function(product) {
+					//remove item by index
 					this.cart.splice(this.cart.indexOf(product), 1)
-					this.total -= product.price;
 				}
 			},
 			controllerAs: 'cartCtl'
